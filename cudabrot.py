55e8e33a56ed0492d3b09ea3049198474478bc79
george <georget415@gmail.com>
2022-10-08 23:25:55 +1100

    * removed unused function

--- cudabrot.py
+++ cudabrot.py
@@ -40,22 +40,6 @@ def julia(x, y, max_iters):
 mandel_gpu = cuda.jit(uint32(f8, f8, uint32), device=True)(mandel)
 julia_gpu = cuda.jit(uint32(f8, f8, uint32), device=True)(julia)
 
-@jit
-def create_fractal(min_x, max_x, min_y, max_y, image, iters):
-    height = image.shape[0]
-    width = image.shape[1]
-
-    pixel_size_x = (max_x - min_x) / width
-    pixel_size_y = (max_y - min_y) / height
-
-    for x in range(width):
-        real = min_x + x * pixel_size_x
-        for y in range(height):
-            imag = min_y + y * pixel_size_y
-            color = mandel(real, imag, iters)
-            image[y, x] = color
-
-
 @cuda.jit((f8, f8, f8, f8, uint8[:,:], uint32))
 def mandel_kernel(min_x, max_x, min_y, max_y, image, iters):
     height = image.shape[0]
